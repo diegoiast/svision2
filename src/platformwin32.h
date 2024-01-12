96b618b4842797d0d2be9a79fc2fe5ad320f1072
Diego Iastrubni <diegoiast@gmail.com>
2024-01-12 09:45:38 +0200

    PlatformWin32: mark function as override

--- src/platformwin32.h
+++ src/platformwin32.h
@@ -20,7 +20,7 @@ struct PlatformWin32 : Platform {
     virtual auto show_window(std::shared_ptr<PlatformWindow> window) -> void override;
 
     virtual auto set_cursor(PlatformWindow &window, MouseCursor cursor) -> void override;
-    virtual auto invalidate(PlatformWindow &window) -> void;
+    virtual auto invalidate(PlatformWindow &window) -> void override;
     virtual auto main_loop() -> void override;
 };
 
